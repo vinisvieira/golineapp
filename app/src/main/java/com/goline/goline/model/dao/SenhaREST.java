58112c637fb4f7bc176fd1b0398653f8a6f1b9a1
Vinicius <lindemberg.vinicius@gmail.com>
2016-12-08 10:57:42 -0300

    ultimo commit

--- app/src/main/java/com/goline/goline/model/dao/SenhaREST.java
+++ app/src/main/java/com/goline/goline/model/dao/SenhaREST.java
@@ -28,7 +28,8 @@ public class SenhaREST {
         try {
 
             //urlConnection = new URL("http://192.168.25.5:8080/GoLine_1.0/PegarSenhaGson?id="+id);
-            urlConnection = new URL("http://10.0.3.2:8080:8080/GoLine_1.0/PegarSenhaGson?id="+id);
+            urlConnection = new URL("http://10.82.103.68:8080/GoLine_1.0/PegarSenhaGson?id="+id);
+            //urlConnection = new URL("http://10.0.3.2:8080:8080/GoLine_1.0/PegarSenhaGson?id="+id);
 
             httpURLConnection = (HttpURLConnection) urlConnection.openConnection();
             httpURLConnection.setReadTimeout(15 * SECONDS);
